b21cb211b46e33505f732dfa0e34ca9d2fe73da9
Adelinbrianna <142956482+Adelinbrianna@users.noreply.github.com>
2023-11-18 00:18:49 +0530

    Add files via upload

---
+++ ex_6a/WEB-INF/classes/p5.java
@@ -0,0 +1,31 @@
+/*to set cookies*/
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+public class p5 extends HttpServlet {
+   public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+   {
+     try{
+        response.setContentType("text/html");
+
+        PrintWriter pwriter = response.getWriter();
+
+        String in_data = request.getParameter("userin");
+        
+        Cookie c = new Cookie("Ucookie",in_data);      
+
+        response.addCookie(c);
+
+        pwriter.print("<h3>Hello - The set cookie is:</h3>"+in_data);
+
+        pwriter.println("<br /> This is how cookie is SET");
+
+        //pwriter.print("<a href='Sessions2'>view details</a>");
+      
+        pwriter.close();
+    }
+     catch(Exception exp){
+       System.out.println(exp);
+     }
+  }
+}
